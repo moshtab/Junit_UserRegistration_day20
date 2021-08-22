6c63dc543796ce3856251215de1c795d90ec8c34
moshtab <mdmohsin41439@gmail.com>
2021-08-22 20:05:35 +0530

    added all methods

--- src/main/java/basic/userRegistration/UserRegistration.java
+++ src/main/java/basic/userRegistration/UserRegistration.java
@@ -1,5 +1,8 @@
 package basic.userRegistration;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	public static void main(String[] args) {
 
@@ -26,7 +29,31 @@ public class UserRegistration {
 			return null;
 
 		}
+}
 
+	public boolean validateEmailId(String string) {
+		String emailId = string;
+		String regex = "^([a-zA-Z0-90_.+-]+)@([a-z0-9]+).([a-z]{2,4})(.[a-z]{2,4})?$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(emailId);
+		return m.matches();
 	}
+
+	public boolean validateMobileNumberId(String string) {
+		String mobileNumber = string;
+		String regex = "^(91 )[897][0-9]{9}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(mobileNumber);
+		return m.matches();
+	}
+
+	public boolean validatePasswordId(String string) {
+		String password = string;
+		String regex = "[a-zA-Z0-9]{7,}#"; 
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(password);
+		return m.matches();
+	}
+	
 
 }
--- src/test/java/basic/userRegistration/UserRegistrationTest.java
+++ src/test/java/basic/userRegistration/UserRegistrationTest.java
@@ -1,29 +1,45 @@
 package basic.userRegistration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class UserRegistrationTest {
-	UserRegistration obj;
+	UserRegistration myobj;
 
 	@Before
 	public void initialize() {
-		obj = new UserRegistration();
+		myobj = new UserRegistration();
 
 	}
 
 	@Test
 	public void shouldReturnTheFirstCharacterOfFirstNameInCapitalAndLengthGreaterThanTwo() {
-		assertEquals("Mohammad", obj.firstName("Mohammad"));
+		assertEquals("Mohammad", myobj.firstName("Mohammad"));
 
 	}
 
 	@Test
 	public void shouldReturnTheFirstCharacterOfLastNameInCapitalAndLengthGreaterThanTwo() {
-		assertEquals("Mohsin", obj.lastName("Mohsin"));
+		assertEquals("Mohsin", myobj.lastName("Mohsin"));
 
 	}
+	@Test
+	public void shouldReturnTrueIfEmailIsValid() {
+		assertTrue(myobj.validateEmailId("mdmohsin41439@gmail.com"));
+	}
+	
+	@Test
+	public void shouldReturnTrueIfMobileNumberIsValid() {
+		assertTrue(myobj.validateMobileNumberId("91 9908514276"));
+	}
+	
+	@Test
+	public void shouldReturnTrueIfPasswordIsValid() {
+		assertTrue(myobj.validatePasswordId("Md78678#"));
+	}
+	
 
 }
